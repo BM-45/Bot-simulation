8cd83b3fe472a09dff469b5e6f8c538e6e946da7
bm-45 <bhanumallik.tallapragada@slu.edu>
2025-08-11 16:10:42 +0530

    initial commit

---
+++ src/rover_launch/launch/display_rover.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    urdf_pkg_arg = DeclareLaunchArgument(
+            'urdf_package', default_value='rover_description',
+            description='Package that contains the URDF/Xacro'
+            )
+    urdf_relpath_arg = DeclareLaunchArgument(
+            'urdf_relative_path', default_value='urdf/rover.urdf',
+            description='Path to URDF/Xacro within that package'
+            )
+
+    urdf_package = LaunchConfiguration('urdf_package')
+    urdf_relative_path = LaunchConfiguration('urdf_relative_path')
+
+    urdf_path = PathJoinSubstitution([FindPackageShare(urdf_package), urdf_relative_path])
+
+    robot_description = Command(['xacro ', urdf_path])
+
+    return LaunchDescription([
+        urdf_pkg_arg,
+        urdf_relpath_arg,
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+            ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+            ),
+        Node(
+            package='rviz2',
+            executable='rviz2'
+            ),
+        ])
+
